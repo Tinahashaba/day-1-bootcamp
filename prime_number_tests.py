58591bd7df2dd1f6157f3d139fa1e82105724ab5
Tinah Ashaba <tinahashaba@gmail.com>
2017-07-03 13:45:29 +0100

    Tests added to check whether the returned value is a list and raise an error if the argument is a string

--- prime_number_tests.py
+++ prime_number_tests.py
@@ -1,5 +1,13 @@
 import unittest
+from prime_numbers import prime_number
 
 
-class TestForPrimes(unittest.TestCase):
-    pass
+class TestForPrimeNumbers(unittest.TestCase):
+    def test_raises_error_if_arg_is_a_string(self):
+        self.assertRaises(ValueError,prime_number, "2")
+
+    def test_function_returns_a_list(self):
+        result = prime_number(10)
+        self.assertEqual(type(list), result, "A list is not returned")
+
+
